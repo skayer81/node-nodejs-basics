13142080a762833ae82fab02a21cb1dab7d37c9e
Sergey <seleznev-krista@mail.ru>
2024-10-01 01:52:10 +0300

    feat: add create() script

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { resolve, join } from 'node:path';
+import { open } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
-};
+    const path = resolve();
+    const filePath = join(path, 'src','fs', 'files','fresh.txtc');
+
+    try {
+        const fileHandle = await open(filePath, 'wx'); 
+        await fileHandle.writeFile('I am fresh and young')
+        await fileHandle.close(); 
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+    }
+}
 
 await create();
