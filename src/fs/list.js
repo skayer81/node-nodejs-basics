6371dccb9440ce45e727caf0d0804af28f777726
Sergey <seleznev-krista@mail.ru>
2024-10-01 02:11:38 +0300

    feat: add list()

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,36 @@
+import { readdir, stat } from 'node:fs/promises';
+import { resolve, extname , join} from 'node:path';
+
 const list = async () => {
-    // Write your code here 
+
+    const path = resolve();
+    const filePath = join(path, 'src','fs', 'files');
+
+    readdir(filePath, { withFileTypes: true })
+    .then((result) => {
+      for (const file of result) {
+        if (file.isFile()) {
+          stat(resolve(filePath, file.name))
+            .then((stat) => {
+              console.log(
+                `${String(file.name).slice(
+                  0,
+                  String(file.name).lastIndexOf('.'),
+                )} - ${String(extname(file.name)).slice(1)} - ${stat.size}b`,
+              );
+            });
+        }
+      }
+    }).catch((err) => {
+        if (err.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+    })
+    ;
 };
 
 await list();
+
+
