17ebe99053c6c527111373c05c63122de77f2529
Sergey <seleznev-krista@mail.ru>
2024-10-02 00:37:10 +0300

    feat: add write() streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+
+import { stdout } from 'node:process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { resolve, join } from 'node:path';
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const dir = join(dirname(fileURLToPath(import.meta.url)), 'files')
+    const stream = createWriteStream(resolve(dir, 'fileToWrite.txt'));
+    
+    stdout.write('enter the text to write to the file (Ctrl+C for exit):');
+    
+    process.stdin.on('data', (data) => {
+       stream.write(data);
+    });
 };
 
 await write();
