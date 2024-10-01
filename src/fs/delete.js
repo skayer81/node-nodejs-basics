38ce81a8eab06838033dde6a5138c944f0c3395f
Sergey <seleznev-krista@mail.ru>
2024-10-01 03:05:12 +0300

    feat: add delete()

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { resolve, join } from 'node:path';
+import { unlink } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    unlink(join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToRemove.txt'))
+     .then()
+     .catch((err) => {
+        if (err.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+    })
 };
 
 await remove();
+
+// fileToRemove.txt
