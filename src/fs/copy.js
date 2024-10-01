b8325d3100f9b596c4598e05c5174f6de69965f6
Sergey <seleznev-krista@mail.ru>
2024-10-01 03:54:41 +0300

    feat: add copy()

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { resolve, join } from 'node:path';
+
+const oldDir = join(dirname(fileURLToPath(import.meta.url)), 'files')
+const newDir = join(dirname(fileURLToPath(import.meta.url)), 'files-copy');
+
 const copy = async () => {
-    // Write your code here 
-};
+   
+    try{
+      await mkdir(newDir)
+      const rd = await readdir(oldDir, { withFileTypes: true })
+               .then((result) => {
+            for (const file of result) {
+              if (file.isFile()) {
+                copyFile(
+                  resolve(oldDir, file.name),
+                  resolve(newDir, file.name),
+                );
+              }
+            }
+          });
+    }
+    catch (err){
+        if (err.code === 'EEXIST' || err.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+    } 
+}     
 
 await copy();
