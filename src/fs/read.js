6cc787b06851725f7f9d2fb7d6834ea8a4ed2aec
Sergey <seleznev-krista@mail.ru>
2024-10-01 04:36:10 +0300

    feat add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { join } from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToRead.txt')
+
+    try {
+        const result = await readFile(filePath)
+        console.log(result.toString())
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+    }
 };
 
 await read();
+
+
+
