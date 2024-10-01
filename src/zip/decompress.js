3dfacec9fc7475936f65d59939ea87ed6172b52a
Sergey <seleznev-krista@mail.ru>
2024-10-02 01:09:40 +0300

    feat: add decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { join,dirname } from 'node:path';
+
 const decompress = async () => {
-    // Write your code here 
+    const fileName = 'fileToCompress.txt';
+    const archiveName = 'archive.gz';
+    const dir = join(dirname(fileURLToPath(import.meta.url)), 'files')
+    const gzip = createGunzip();
+    const source = createReadStream(join(dir, archiveName));
+    const destination = createWriteStream(join(dir, fileName));
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 };
 
 await decompress();
