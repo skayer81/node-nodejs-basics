adcacf9a415de802e3254df0928220841428f5f0
Sergey <seleznev-krista@mail.ru>
2024-10-01 20:26:30 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import { join } from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { constants, rename as _rename, access} from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const filePath = join(dirname(fileURLToPath(import.meta.url)), 'files', 'wrongFilename.txt')
+    const newPath = join(dirname(fileURLToPath(import.meta.url)), 'files', 'properFilename.md')
+
+    try {
+        await access(newPath, constants.F_OK)
+          .then(() =>{
+            throw new Error();
+          })
+          .catch((err) => {
+
+          })
+        await _rename(filePath, newPath)
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+    } 
 };
 
 await rename();
+
+
+
