1a95507917cdda2d39024f7a12f4ba0d05e1b9bd
Sergey <seleznev-krista@mail.ru>
2024-10-01 21:51:10 +0300

    feat: add streams read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { resolve, join } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+
+    const dir = join(dirname(fileURLToPath(import.meta.url)), 'files')
+    const stream = createReadStream(resolve(dir, 'fileToRead.txt'));
+
+    stream.on('data', (chank) => {
+        process.stdout.write(chank)
+    }); 
+    
 };
 
 await read();
+
