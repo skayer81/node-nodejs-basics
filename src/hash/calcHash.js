5503db9d0ccfe4055b519cfb0dea63cdaf274db2
Sergey <seleznev-krista@mail.ru>
2024-10-02 02:59:53 +0300

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = 'fileToCalculateHashFor.txt';
+    const dir = join(dirname(fileURLToPath(import.meta.url)), 'files');
+    const hash = createHash('sha256');
+    const fileStream = createReadStream(join(dir, fileName));
+    fileStream
+    .on('data', (chunk) => {
+        hash.update(chunk);
+    })
+    .on('end', () => {
+       process.stdout.write(hash.digest('hex'))
+    })
 };
 
 await calculateHash();
