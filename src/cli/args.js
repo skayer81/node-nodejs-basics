64831d160a6488b74c3f2fd3d75de917e531a9c8
Sergey <seleznev-krista@mail.ru>
2024-10-03 19:40:42 +0300

    feat: add args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    const argPrefix = '--';
+    const result = [];
+
+    for (let i = 0; i < process.argv.length; i++ ){
+        const item = process.argv[i];
+        if (item.startsWith(argPrefix) && process.argv[i+1]){
+                result.push(`${item} is ${process.argv[i+1]} `)
+                i++;
+            } 
+        }
+    console.log(result.join())
 };
 
 parseArgs();
